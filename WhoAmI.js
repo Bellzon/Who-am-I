487e7ae9f87976fc4b2dd3821f75897809cc5ce4
Bellér Péter <pet.beller@gmail.com>
2018-04-27 23:20:28 +0200

    very first try

---
+++ WhoAmI.js
@@ -0,0 +1,10 @@
+function father(){
+	var x = "más szoveg"
+ var ft = document.getElementById("father-text");
+ if ( ft.style.display === "none"){
+ 	 ft.style.display = "initial";
+ } else {
+ 	ft.style.display = "none";
+ }
+
+}
